4b8d1118af781d0445540b699526aab76e854254
Moirted <moirted@gmail.com>
2022-10-26 15:59:51 +0300

    micro

--- Rakefile
+++ Rakefile
@@ -5,7 +5,7 @@
 # 5. Implement pawn moves
 # 6. Implement getting a new piece when a pawn reaches the end
 # 7. Implement the logic of the game itself, add players
-# 8. Make a check for check and end the game
+# 8. Make a check for checkmate and end the game
 # 9. Write tests and test everything
 
 # TODO START TESTS
--- bin/chess-engine_main.rb
+++ bin/chess-engine_main.rb
@@ -13,18 +13,6 @@ g = Game.new(
   BoardRenderText
 )
 g.play
-# b = Board.new
-# b[[0,1]] = Rook.new(b, [0,1], :black)
-# b[[1,0]] = Pawn.new(b, [1,0], :white)
-# b[[6,2]] = King.new(b, [6,2], :black)
-# b[[4,4]] = King.new(b, [4,4], :white)
-#
-#  g = Game.new(
-#    b,
-#    Player.new(:white),
-#    Player.new(:black),
-#    BoardRenderText
-#  )
-#  g.play
+
 
 # 1. e4 e5 2. Qh5?! Nc6 3. Bc4 Nf6?? 4. Qxf7# – это запись в английской нотации
